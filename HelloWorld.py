d716a3532737ca715396e8b12c92f60bf0d1aaeb
Patrick Tomei <Patrick Tomei>
2017-05-26 14:13:16 +0900

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,16 @@
+#greeting = "Hello"
+#name = input("Please enter your name ")
+#print(greeting + ' ' + name)
+
+# splitstring = "This string has been\nsplit over\nseveral\nlines"
+# print(splitstring)
+#
+# tabbedstring = "1\t2\t3\t4\t5\t"
+# print(tabbedstring)
+print('The pet shop owner said, "No, no, \'e\'s uh,...he\'s resting"')
+print("The pet shop owner said, \"No, no, 'e's uh,...He's resting\"")
+
+anothersplitsring = """This string has been 
+split over
+several lines"""
+print(anothersplitsring)
---
+++ Variable.py
@@ -0,0 +1,43 @@
+# a = 12
+# b = 3
+#
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+#
+# print((((a + b) / 3) - 4) * 12)
+#
+# c = a + b
+# d = c / 3
+# e = d - 4
+# print(e * 12)
+#
+# parrot = "Norwegian Blue"
+# print(parrot)
+# print(parrot[3])
+# print(parrot[-1])
+# print(parrot[0:6])
+# print(parrot[:6])
+# print(parrot[6:])
+# print(parrot[-4:-2])
+# print(parrot[0:6:2])
+# print(parrot[0:6:3])
+#
+# number = "9,223,372,036,854,775,807"
+# print(number[1::4])
+#
+# numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+# print(numbers[0::3])
+
+string1 = "he's "
+string2 = "probably "
+print(string1 + string2)
+
+print("Hello " * 5)
+
+today = "friday"
+print("day" in today)
+
